f407084544b3596a93ecaf0c5f3d8bbce2be026c
PedroVictor011 <oliveira.pedrovictor2@gmail.com>
2024-11-05 17:48:47 -0300

    Add files via upload

---
+++ porva_02.py
@@ -0,0 +1,11 @@
+    
+usuario = int(input ('digite um numero:'))
+
+if usuario == 0:
+    print('o numero e zero')
+
+elif usuario <0:
+    print('o numero e negativo')
+
+else:
+    print('o numero e positivo')
